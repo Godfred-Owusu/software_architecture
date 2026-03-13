3ae5709aa0784d2d752677869c0b7da59b91a018
godfr <gowusu018@gmail.com>
2026-03-13 16:02:17 +0100

    chore: finalize all DTO validations and api documentation properties

--- src/modules/shared/auth/application/dtos/login.dto.ts
+++ src/modules/shared/auth/application/dtos/login.dto.ts
@@ -1,4 +1,17 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+
 export class LoginDto {
+  @ApiProperty({ example: 'johndoe', description: 'The username of the user' })
+  @IsString()
+  @IsNotEmpty({ message: 'Username is required' })
   username: string;
+
+  @ApiProperty({
+    example: 'password123',
+    description: 'The password of the user',
+  })
+  @IsString()
+  @IsNotEmpty({ message: 'Password is required' })
   password: string;
 }
